d5fe16f6e15556ddb68f1cd02ab40012aeae5fac
keegels <keegansethpena@gmail.com>
2025-11-10 19:35:43 -0600

    Create Link.php

---
+++ Link.php
@@ -0,0 +1,22 @@
+<?php
+    class Link
+    {
+        private value = null;
+        private Link next = null;
+        private Link previous = null;
+
+        __construct($value)
+        {
+            $this->value = $value;
+        }
+
+        protected function setNext(Link $next)
+        {
+            $this->next = $next;
+        }
+
+        protected function setPrevious(Link $previous)
+        {
+            $this->previous = $previous;
+        }
+    }
